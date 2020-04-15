f8d36902ecbc7d13d88485254cbc25367538f286
Nebarnix <nebarotype@gmail.com>
2020-04-15 08:09:10 -0700

    Add files via upload
    Fixed stick to throttle instead of always at full speed. Use L2 as a speed boost. 
    Added Mario Kart mode - press R2 to move forward and use the left stick to turn left and right (front and back are ignored). Release R2 to use full stick control for zero point turns and fast motion changes!

--- HSL_Robot_PS2.ino
+++ HSL_Robot_PS2.ino
@@ -1,7 +1,13 @@
-//HeatSyncLabs Robot PS2X Controller 
+//HeatSyncLabs Robot PS2X Controller
 //SPECIAL THANKS TO BILL PORTER FOR BEING SO AWESOME
 
 #include <PS2X_lib.h>  //for v1.6
+#include <NeoSWSerial.h>
+
+#define RF_TX_PIN 7
+#define RF_RX_PIN 8
+NeoSWSerial Serial1(RF_TX_PIN, RF_RX_PIN);
+
 
 PS2X ps2x; // create PS2 Controller Class
 
@@ -21,8 +27,9 @@ unsigned long int timeD2 = 0;
 String cmd = "";
 
 void setup() {
-  Serial.begin(9600);
-
+  Serial.begin(9600);  
+  Serial1.begin(9600);  
+  
   //CHANGES for v1.6 HERE!!! **************PAY ATTENTION*************
   //GamePad(clock, command, attention, data, Pressures?, Rumble?)
   //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
@@ -71,12 +78,12 @@ void loop() {
   */
 
   int throttle = 100;
-  
+
 
   if (error == 1) //skip loop if no controller found
     return;
 
-  /*
+/*
     //DualShock Controller
 
     //ps2x.read_gamepad(small motor on/off, larger motor strenght from 0-255)
@@ -151,28 +158,77 @@ void loop() {
         Serial.print(" ");
         Serial.println(ps2x.Analog(PSS_RX), DEC);
     }
-  */
+  
+  return; */
+  
   ps2x.read_gamepad(false, vibrate);          //read controller and set large motor to spin at 'vibrate' speed
 
   if (ps2x.Button(PSB_L2)) //L2 speed boost
     throttle = 255;
 
-  //if there no command waiting then look for buttons. 
+  //if there no command waiting then look for buttons.
   //Else wait for the command to clear (or we might miss brief key presses!)
 
-  if (cmd == "") 
+  if (cmd == "")
   {
-    if (ps2x.Button(PSB_PAD_UP))        //will be TRUE as long as button is pressed
+    //This section is for movement commands. Anything that falls out at the end will result in the STOP command being issued to halt runaway motion.
+    if (ps2x.Button(PSB_R2)) //mario kart style R2 accel button
     {
-      cmd  += "FWD=";
-      cmd  += throttle;
+    cmd += "RAW=";
+      cmd += throttle;
+      cmd += ",";
+      cmd += map(ps2x.Analog(PSS_LX),0,255,int(-throttle*0.65),int(throttle*0.65)); //limit side to side motion for controllability in mario kart mode
+      cmd  += ';';  
+    }
+    else if (ps2x.Analog(PSS_LY) > (127+15) || ps2x.Analog(PSS_LY) < (127-15) || ps2x.Analog(PSS_LX) > (127+15) || ps2x.Analog(PSS_LX) < (127-15) )
+    {
+      cmd += "RAW=";
+      cmd += map(ps2x.Analog(PSS_LY),255,0,-throttle,throttle);
+      cmd += ",";
+      cmd += map(ps2x.Analog(PSS_LX),0,255,-throttle,throttle);
       cmd  += ';';
+    }
+    else if (ps2x.Button(PSB_PAD_UP))        //will be TRUE as long as button is pressed
+    {
+      if (ps2x.Button(PSB_PAD_LEFT))
+      {
+        cmd  += "FWL=";
+        cmd  += throttle;
+        cmd  += ';';
+      }
+      else if (ps2x.Button(PSB_PAD_RIGHT))
+      {
+        cmd  += "FWR=";
+        cmd  += throttle;
+        cmd  += ';';
+      }
+      else
+      {
+        cmd  += "FWD=";
+        cmd  += throttle;
+        cmd  += ';';
+      }
     }
     else if (ps2x.Button(PSB_PAD_DOWN))        //will be TRUE as long as button is pressed
     {
-      cmd  += "REV=";
-      cmd  += throttle;
-      cmd  += ';';
+      if (ps2x.Button(PSB_PAD_LEFT))
+      {
+        cmd  += "RVL=";
+        cmd  += throttle;
+        cmd  += ';';
+      }
+      else if (ps2x.Button(PSB_PAD_RIGHT))
+      {
+        cmd  += "RVR=";
+        cmd  += throttle;
+        cmd  += ';';
+      }
+      else
+      {
+        cmd  += "REV=";
+        cmd  += throttle;
+        cmd  += ';';
+      }
     }
     else if (ps2x.Button(PSB_PAD_LEFT))    //will be TRUE as long as button is pressed
     {
@@ -186,18 +242,39 @@ void loop() {
       cmd  += int(constrain(throttle * turnMultiplier, 0, 255));
       cmd  += ';';
     }
-  else
-    cmd += "STP;";
-    
-  if (ps2x.Button(PSB_L1)) //L2 speed boost
-     {
+    else
+      cmd += "STP;";
+
+    //Other commands NOT pertaining to movement
+    if (ps2x.Analog(PSS_RY) > (127+15) || ps2x.Analog(PSS_RY) < (127-15) || ps2x.Analog(PSS_RX) > (127+15) || ps2x.Analog(PSS_RX) < (127-15) )
+    {
+      cmd += "SRV=";
+      cmd += map(ps2x.Analog(PSS_RY),127,0,0,90); //map center to 0
+      cmd += ",";
+      cmd += map(ps2x.Analog(PSS_RX),255,0,0,180); //map center to 90
+      cmd  += ';';
+    }
+    else if(ps2x.Button(PSB_R3))
+      cmd  += "SRV=0,90;";
+
+    if (ps2x.Button(PSB_L1)) //L1 Headlight Toggle
       cmd  += "HLO;";
-      }
+
+    if (ps2x.Button(PSB_BLUE)) //R1 "Not cool man!"
+      cmd  += "NCM;";
+    else if (ps2x.Button(PSB_PINK)) //R1 "Look at this!"
+      cmd  += "LAT;";
+    else if (ps2x.Button(PSB_RED)) //R1 Insult
+      cmd  += "TNT;";
+    else if (ps2x.Button(PSB_GREEN)) //R1 Insult
+      cmd  += "CMP;";
+    
   }
-   
+
   if ((millis() - theTime) > 100) //loop constantly, but only send radio commands every so often
   {
     Serial.print(cmd);
+    Serial1.print(cmd);
     cmd = "";
     theTime = millis();
   }
